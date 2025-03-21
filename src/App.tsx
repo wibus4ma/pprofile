8c7ae71a8dc4f0cc28af90036ab0b2bb4f850292
wibusama <wibusama2104@gmail.com>
2025-03-21 19:11:21 +0700

    fix tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -18,22 +18,6 @@ function App() {
           <p className="absolute right-0 bottom-[42%] underline cursor-pointer">
             wibus4ma?
           </p>
-          <div className="relative inline-block">
-            <button
-              className="bg-primary inline-flex rounded text-base font-semibold text-black whitespace-nowrap"
-              data-tooltip-target="top-tooltip"
-            >
-              Tooltip on top
-            </button>
-            <div
-              className="absolute bottom-full invisible left-1/2 z-20 mb-3 -translate-x-1/2 whitespace-nowrap rounded-md bg-white py-2 px-4 text-xs text-gray-800 font-medium transition-opacity duration-300 border border-gray-300 shadow-[0px_12px_30px_-4px_rgba(16,24,40,0.08);]"
-              role="tooltip"
-              id="top-tooltip"
-            >
-              <span className="absolute -bottom-1.5 left-1/2 -z-10 h-3 w-3 border-b border-r border-gray-300 -translate-x-1/2 rotate-45  bg-white"></span>
-              This is a tooltip
-            </div>
-          </div>
         </div>
         <div className="min-w-[320px] mb-4">
           <ul className="flex justify-center space-x-4 border rounded-l-full rounded-r-full p-2">
